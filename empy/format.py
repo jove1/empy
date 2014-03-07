a35fef8d855a89c80ff0a57840847e1bc68500a0
Jozef Vesely <vesely@gjh.sk>
2014-03-07 10:48:03 +0100

    add unicode format
    unfortunately it is not working very well

--- empy/format.py
+++ empy/format.py
@@ -1,11 +1,25 @@
 
-def fancy(v):
-    v = ("\\bar{{{:g}}}".format(-x) if x<0 else "{:g}".format(x) for x in v)
-    return "${0}{1}{2}$".format(*v)
-
 def simple(v):
     return "{0:g}{1:g}{2:g}".format(*v)
 
+def latex(v):
+    pos = "{:g}".format
+    neg = r"\bar{{{:g}}}".format
+    return "${0}{1}{2}$".format(*[neg(-x) if x<0 else pos(x) for x in v])
+
 def html(v):
-    v = ("<span style='text-decoration: overline;'>{:g}</span>".format(-x) if x<0 else "{:g}".format(x) for x in v)
-    return "{0}{1}{2}".format(*v)
+    pos = "{:g}".format
+    neg = "<span style='text-decoration: overline;'>{:g}</span>".format
+    return "{0}{1}{2}".format(*[neg(-x) if x<0 else pos(x) for x in v])
+
+def unicode(v):
+    pos = "{:g}".format
+    def neg(x):
+        overline = u"\u0305"
+        macron =  u"\u0304"
+        x = pos(x)
+        if len(x) == 1:
+            return x+macron
+        else:
+            return u"".join(sum(zip(x,overline*len(x)),()))
+    return u"{0}{1}{2}".format(*[neg(-x) if x<0 else pos(x) for x in v])
